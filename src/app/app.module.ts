fa81cf18e4667edeb57b21ef0dbf6b655b240129
prograTammz <ahmedtamer1998@gmail.com>
2019-06-24 15:00:03 +0200

    Added router Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
      MatCheckboxModule,
+     AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
