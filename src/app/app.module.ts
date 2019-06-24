4f6f00efcc03324c8da085ffc4aa3450ec1c7bad
prograTammz <ahmedtamer1998@gmail.com>
2019-06-24 15:38:04 +0200

    imported Advices/dashboard pages to app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AdvicesPageComponent } from './advices-page/advices-page.component';
+import { AdvicesPageComponent } from './pages/advices-page/advices-page.component';
+import { AboutusPageComponent } from './pages/aboutus-page/aboutus-page.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AdvicesPageComponent
+    AdvicesPageComponent,
+    AboutusPageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
