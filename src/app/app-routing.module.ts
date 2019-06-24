24dccff7e0b1216bf78ac469ea4f5ee45df6586c
prograTammz <ahmedtamer1998@gmail.com>
2019-06-24 15:44:56 +0200

    Adding "Advice, Aboutus & Dashboard" to router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule, Routes, Router} from '@angular/router';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {AdvicesPageComponent} from './pages/advices-page/advices-page.component';
+import {AboutusPageComponent} from './pages/aboutus-page/aboutus-page.component';
 
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', component: DashboardComponent ,pathMatch: 'full'},
+  {path: 'advices', component: AdvicesPageComponent},
+  {path: 'aboutus', component:AboutusPageComponent}
+]
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
