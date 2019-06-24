9be03e9c3d1a0d9bf38f80e7d587bbc9d8dcac84
prograTammz <ahmedtamer1998@gmail.com>
2019-06-24 18:03:12 +0200

    Dynamically load page name, menu & back buttons func

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Location} from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { NavigationEnd,Router,ActivatedRoute } from '@angular/router';
+import {filter,map}from 'rxjs/operators'
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -8,9 +9,31 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private location: Location, private activatedRoute: ActivatedRoute) { }
+  private title : string
+  constructor(private router: Router,private location: Location, private activatedRoute: ActivatedRoute) { 
+    
+    
+  }
 
   ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(()=>{
+        let child = this.activatedRoute.firstChild;
+          while (child) {
+            if (child.firstChild) {
+              child = child.firstChild;
+            } else if (child.snapshot.data && child.snapshot.data['routeName']) {
+              return child.snapshot.data['routeName'];
+            } else {
+              return null;
+            }
+          }
+          return null;
+      })
+    ).subscribe((data:any)=>{
+      this.title = data;
+    })
   }
   openLoginModal(): void{
     console.log("Testing LoginModal");
