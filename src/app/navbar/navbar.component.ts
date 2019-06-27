4f166ee5f83d4e12acd74db6ceeebe65a5b00796
prograTammz <ahmedtamer1998@gmail.com>
2019-06-27 07:04:26 +0200

    Added the material side-nav with dummy data

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,6 +9,13 @@ import {filter,map}from 'rxjs/operators'
 })
 export class NavbarComponent implements OnInit {
 
+  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+  fillerContent = Array.from({length: 50}, () =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
   private title : string
   constructor(private router: Router,private location: Location, private activatedRoute: ActivatedRoute) { 
     
