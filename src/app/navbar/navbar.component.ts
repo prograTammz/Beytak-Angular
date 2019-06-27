17d0332f0195a7ec71e2cbdb1f0d9a88888d36d4
prograTammz <ahmedtamer1998@gmail.com>
2019-06-27 07:49:24 +0200

    Refactored tradiation icons to mat-icons

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Location} from '@angular/common';
 import { NavigationEnd,Router,ActivatedRoute } from '@angular/router';
-import {filter,map}from 'rxjs/operators'
+import {filter,map}from 'rxjs/operators';
+import { MatIconRegistry } from '@angular/material/icon';
+import {DomSanitizer} from '@angular/platform-browser';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -17,9 +19,11 @@ export class NavbarComponent implements OnInit {
        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
   private title : string
-  constructor(private router: Router,private location: Location, private activatedRoute: ActivatedRoute) { 
-    
-    
+  constructor(private sanitizer: DomSanitizer,private matIconRegistry: MatIconRegistry,private router: Router,private location: Location, private activatedRoute: ActivatedRoute) { 
+    this.matIconRegistry.addSvgIcon('menu', sanitizer.bypassSecurityTrustResourceUrl('../../assets/icons/menu.svg'));
+    this.matIconRegistry.addSvgIcon('close', sanitizer.bypassSecurityTrustResourceUrl('../../assets/icons/close.svg'));
+    this.matIconRegistry.addSvgIcon('search', sanitizer.bypassSecurityTrustResourceUrl('../../assets/icons/search.svg'));
+    this.matIconRegistry.addSvgIcon('fav', sanitizer.bypassSecurityTrustResourceUrl('../../assets/icons/fav.svg'));
   }
 
   ngOnInit() {
